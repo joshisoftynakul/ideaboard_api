09697ef264112704d53b7f1a980c32dcba25e9e8
joshisoftynakul <joshisoftynakul@gmail.com>
2017-11-18 17:02:45 +0530

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+ideas = Idea.create(
+	[
+		{
+			title: "Pancake recipe",
+			body: "Batter + Mapel Syrup"
+		},
+		{
+			title: "Portfolio Website",
+			body: "Get photos clicked"
+		},
+		{
+			title: "YouTube",
+			body: "Watch Roadman Shaq Videos"
+		},
+		{
+			title: "Facebook",
+			body: "Remove Stories"
+		}
+	]
+)
